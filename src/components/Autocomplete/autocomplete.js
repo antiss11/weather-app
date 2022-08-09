9a8154b62ca2e8744f901823c95cb903833bc867
Dmitry Kulik <recc1337@gmail.com>
2022-08-09 12:40:00 +0300

    Add index and autocomplete's search bar with styles

---
+++ src/components/Autocomplete/autocomplete.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { GeocoderAutocomplete } from "@geoapify/geocoder-autocomplete";
+import "./autocomplete.css";
+
+function autocomplete(API_KEY, root) {
+  const autocomplete = new GeocoderAutocomplete(root, API_KEY, {
+    type: "city",
+  });
+
+  autocomplete.setPostprocessHook((feature) => {
+    return `${feature.properties.address_line1} ${feature.properties.address_line2}`;
+  });
+
+  return autocomplete;
+}
+
+export default autocomplete;
