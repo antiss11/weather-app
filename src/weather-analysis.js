7a1a31e13b9a64f31b2ea806583f726e7037b8ad
Dmitry Kulik <recc1337@gmail.com>
2022-08-10 18:02:33 +0300

    Add functions to calculate weather and get icons by weathercodes

---
+++ src/weather-analysis.js
@@ -0,0 +1,111 @@
+import {
+  WiDaySunny,
+  WiDayCloudy,
+  WiDaySunnyOvercast,
+  WiDayFog,
+  WiDayRain,
+  WiDayThunderstorm,
+  WiDayShowers,
+  WiDaySnow,
+  WiNightClear,
+  WiNightCloudy,
+  WiNightAltCloudy,
+  WiNightFog,
+  WiNightRain,
+  WiNightThunderstorm,
+  WiNightShowers,
+  WiNightSnow,
+} from "weather-icons-react";
+
+const weatherCodes = {
+  0: "Clear sky",
+  1: "Mainly clear",
+  2: "Partly cloudy",
+  3: "Overcast",
+  45: "Fog",
+  48: "Depositing rime fog",
+  51: "Light drizzle",
+  53: "Moderate drizzle",
+  55: "Dense drizzle",
+  61: "Slight rain",
+  63: "Moderate rain",
+  65: "Heavy rain",
+  66: "Light freezing rain",
+  67: "Heavy freezing rain",
+  71: "Slight snow fall",
+  73: "Moderate snow fall",
+  75: "Heavy snow fall",
+  77: "Snow grains",
+  80: "Slight rain shower",
+  81: "Moderate rain shower",
+  82: "Violent rain shower",
+  85: "Slight snow shower",
+  86: "Heavy snow showet",
+  95: "Thunderstorm",
+  96: "Thunderstorm with slight hail",
+  99: "Thunderstorm with heavy hail",
+};
+
+function getFormattedForecast(forecastRawData) {
+  return Object.assign(
+    ...forecastRawData.daily.time.map((day, i) => ({
+      [day]: {
+        weatherCode: forecastRawData.daily.weathercode[i],
+        night: Math.round(
+          forecastRawData.hourly.temperature_2m
+            .slice(i * 24, i * 24 + 7)
+            .reduce((sum, temperature) => sum + temperature) / 7 // 7 - hours number between 00:00 AM and 06:00 AM
+        ),
+        day: Math.round(
+          forecastRawData.hourly.temperature_2m
+            .slice(i * 24 + 7, i * 24 + 23)
+            .reduce((sum, temperature) => sum + temperature) / 16 // 16 - hours number between 07:00 AM and 22:00 PM
+        ),
+      },
+    }))
+  );
+}
+
+function getIconByWeatherCode(weatherCode, isDay) {
+  if (isDay) {
+    switch (weatherCode) {
+      case (0, 1):
+        return <WiDaySunny size={24} />;
+      case 2:
+        return <WiDayCloudy size={24} />;
+      case 3:
+        return <WiDaySunnyOvercast size={24} />;
+      case (45, 48):
+        return <WiDayFog size={24} />;
+      case (51, 53, 55, 61, 63, 65, 66, 67):
+        return <WiDayRain size={24} />;
+      case (71, 73, 75, 77):
+        return <WiDaySnow size={24} />;
+      case (80, 81, 82, 85, 86):
+        return <WiDayShowers size={24} />;
+      case (95, 95, 99):
+        return <WiDayThunderstorm size={24} />;
+    }
+  } else {
+    switch (weatherCode) {
+      case (0, 1):
+        return <WiNightClear size={24} />;
+      case 2:
+        return <WiNightCloudy size={24} />;
+      case 3:
+        return <WiNightAltCloudy size={24} />;
+      case (45, 48):
+        return <WiNightFog size={24} />;
+      case (51, 53, 55, 61, 63, 65, 66, 67):
+        return <WiNightRain size={24} />;
+      case (71, 73, 75, 77):
+        return <WiNightSnow size={24} />;
+      case (80, 81, 82, 85, 86):
+        return <WiNightShowers size={24} />;
+      case (95, 95, 99):
+        return <WiNightThunderstorm size={24} />;
+    }
+  }
+}
+
+export { getFormattedForecast, getIconByWeatherCode, weatherCodes };
