472aa20cc2401d287ec2d72133723e8ef7aa0659
Dmitry Kulik <recc1337@gmail.com>
2022-08-16 05:57:16 +0300

    Split dayforecast component into components

--- src/components/DayForecast/DayForecast.js
+++ src/components/DayForecast/DayForecast.js
@@ -1,42 +1,74 @@
 import React from "react";
-import { Card, CardContent, CardMedia, Divider } from "@mui/material";
-import { Typography } from "@mui/material";
+import {
+  Card,
+  CardContent,
+  Divider,
+  createTheme,
+  ThemeProvider,
+} from "@mui/material";
+import { Typography, Box } from "@mui/material";
 import { getIconByWeatherCode } from "/src/weather-analysis.js";
+import { weatherCodes } from "../../weather-analysis";
+
+const theme = createTheme({
+  typography: {
+    h3: {
+      fontSize: 18,
+    },
+    h4: {
+      fontSize: 16,
+    },
+    weatherVerdict: {
+      fontSize: 14,
+    },
+  },
+});
 
 function DayForecast({ name, forecast }) {
-  console.log(forecast);
   return (
-    <Card sx={{ width: 1 }}>
-      <CardContent>
-        <Typography variant="h5" component="h4" align="center">
-          {name}
-        </Typography>
-        <Divider />
-        <Typography variant="h6" component="h5" align="center">
-          Day:
-        </Typography>
-        <Typography align="center">
-          {getIconByWeatherCode({
-            weatherCode: forecast.weatherCode,
-            isDay: true,
-            iconSize: 60,
-          })}
-          {forecast.day}
-        </Typography>
-        <Divider />
-        <Typography variant="h6" component="h5" align="center">
-          Night:
-        </Typography>
-        <Typography align="center">
-          {getIconByWeatherCode({
-            weatherCode: forecast.weatherCode,
-            isDay: false,
-            iconSize: 60,
-          })}
-          {forecast.night}
-        </Typography>
-      </CardContent>
-    </Card>
+    <ThemeProvider theme={theme}>
+      <Card>
+        <CardContent>
+          <Typography variant="h3" component="h3" align="center">
+            {name}
+          </Typography>
+          <Divider />
+          <Box
+            sx={{ display: "flex", flexDirection: "column", alignItems: "center" }}
+          >
+            {getIconByWeatherCode({
+              weatherCode: forecast.weatherCode,
+              isNight: false,
+              iconSize: 60,
+            })}
+            <Typography variant="weatherVerdict">
+              {weatherCodes[forecast.weatherCode]}
+            </Typography>
+          </Box>
+          <Divider />
+          <DayNightForecast dayName={name} forecast={forecast.day}>
+            Day
+          </DayNightForecast>
+          <Divider />
+          <DayNightForecast dayName={name} forecast={forecast.night}>
+            Night
+          </DayNightForecast>
+        </CardContent>
+      </Card>
+    </ThemeProvider>
+  );
+}
+
+function DayNightForecast(props) {
+  return (
+    <Box sx={{ py: 1 }}>
+      <Typography variant="h4" component="h4" align="center">
+        {props.children}
+      </Typography>
+      <Box>
+        <Typography align="center">{props.forecast} &deg;C</Typography>
+      </Box>
+    </Box>
   );
 }
 
