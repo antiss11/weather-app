e080375130d01dffd07fbffc1996e615d18cd74c
Dmitry Kulik <recc1337@gmail.com>
2022-08-25 10:32:44 +0300

    Set type of config object (fix error)

--- src/config.js
+++ src/config.js
@@ -1,3 +1,5 @@
-export default config = {
-  GEOAPIFY_API_KEY: "5498efe8440a42a3a9e8e329ddfeff33",
+const config = {
+	GEOAPIFY_API_KEY: "5498efe8440a42a3a9e8e329ddfeff33",
 };
+
+export default config;
